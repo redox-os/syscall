af9aa0c3b24dc556df5309034453b048d60773f1
Jeremy Soller <jackpot51@gmail.com>
2016-11-16 20:50:34 -0700

    Add CLONE_NEWNS

--- src/flag.rs
+++ src/flag.rs
@@ -2,15 +2,7 @@ pub const CLONE_VM: usize = 0x100;
 pub const CLONE_FS: usize = 0x200;
 pub const CLONE_FILES: usize = 0x400;
 pub const CLONE_VFORK: usize = 0x4000;
-/// Mark this clone as supervised.
-///
-/// This means that the process can run in supervised mode, even not being connected to
-/// a supervisor yet. In other words, the parent can later on supervise the process and handle
-/// the potential blocking syscall.
-///
-/// This is an important security measure, since otherwise the process would be able to fork it
-/// self right after starting, making supervising it impossible.
-pub const CLONE_SUPERVISE: usize = 0x400000;
+pub const CLONE_NEWNS: usize = 0x8000;
 
 pub const CLOCK_REALTIME: usize = 1;
 pub const CLOCK_MONOTONIC: usize = 4;
