0362efec43e061fe66d39c49f40fa472588c0254
Jeremy Soller <jackpot51@gmail.com>
2017-01-09 20:16:29 -0700

    Add features

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,10 +1,5 @@
 //! I/O functions
 
-#![feature(asm)]
-#![feature(const_fn)]
-#![feature(core_intrinsics)]
-#![no_std]
-
 pub use self::dma::*;
 pub use self::io::*;
 pub use self::mmio::*;
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 #![feature(asm)]
+#![feature(const_fn)]
+#![feature(core_intrinsics)]
 #![no_std]
 
 pub use self::arch::*;
