44d4ead33e01e968afe99a096e4de392c8a6380b
Ian Douglas Scott <ian@iandouglasscott.com>
2017-07-10 16:35:04 -0700

    Add MODE_CHR flag for "character special"

--- src/flag.rs
+++ src/flag.rs
@@ -25,6 +25,7 @@ pub const MODE_TYPE: u16 = 0xF000;
 pub const MODE_DIR: u16 = 0x4000;
 pub const MODE_FILE: u16 = 0x8000;
 pub const MODE_SYMLINK: u16 = 0xA000;
+pub const MODE_CHR: u16 = 0x2000;
 
 pub const MODE_PERM: u16 = 0x0FFF;
 pub const MODE_SETUID: u16 = 0o4000;
